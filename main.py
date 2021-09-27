cf680eab479b2b7db90269f88341c3f66fbecbeb
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-27 18:51:50 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame #allow us to use the module
 width, height = 900, 500
 
 win= pygame.display.set_mode((width, height))
+pygame.display.set_caption('First Game')#title of the game
+white= 255,255,255
 
 
 def main():
@@ -13,4 +15,10 @@ def main():
             if event.type ==pygame.QUIT:
                 run = False
 
+        win.fill((white))
+
     pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
