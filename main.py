ed3525276fa34f6610346767c3170db944c79a5d
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-27 19:33:06 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -15,20 +15,21 @@ SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55,40
 
 
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
-YELLOW_SPACESHIP = pygame.image.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90) #changes the image size
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90) #changes the image size and its position
 
 
 
 
 
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
-RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)) #changes the image size
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)),90) #changes the image size
 
 
 
 def draw_window():
     win.fill((white))
     win.blit(YELLOW_SPACESHIP, (300, 100))#add to the screen
+    win.blit(RED_SPACESHIP, (700, 100))
     pygame.display.update()
 
 
