0b8079928e05eaf0081ce447799dc09d2bb0c679
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-27 19:26:11 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -10,8 +10,17 @@ pygame.display.set_caption('First Game')#title of the game
 white= 255,255,255
 
 fps= 60
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55,40
+
+
+
 YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_yellow.png'))
+YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (55,40)) #changes the image size
+
 RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assets', 'spaceship_red.png'))
+YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (55,40)) #changes the image size
+
+
 
 def draw_window():
     win.fill((white))
